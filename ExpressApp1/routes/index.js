5927e21e600bb707a2e62256d8ad5fca7a5aeb71
Jeremy Almas <jkalmas316@hotmail.com>
2017-05-24 22:34:31 -0400

    Blog API project

---
+++ ExpressApp1/routes/index.js
@@ -0,0 +1,36 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+var BlogPosts = require('../models/models');
+var BlogPostActions = BlogPosts.BlogPosts;
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/blogposts', (req, res) => {
+    res.json(BlogPostActions.get());
+});
+
+router.post('/blogposts', (req, res) => {
+    BlogPostActions.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
+    res.json(BlogPostActions.get());
+});
+
+router.put('/blogpost', (req, res) => {
+    BlogPostActions.update(req.body.blogpost);
+    res.json(BlogPostActions.get());
+});
+
+router.delete('/blogpost/:id', (req, res) => {
+    BlogPostActions.delete(req.params.id);
+    res.json(BlogPostActions.get());
+});
+
+module.exports = router;
+
+//change all the "req, res" to their appropriate names
+//singular or pluralization of a word
+//
+
+
